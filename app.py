ec225b4cc628406082151f604849a167a752526b
levin-mutai <levinmutai2@gmail.com>
2023-02-08 16:38:41 +0300

    finnished send message

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ from messages.resources import send_sms
 import json
 
 
-class Client:
+class message:
 
-    def __init__(self, username, password, senderid, output='json', duplicatecheck='true') -> None:
-        self.userid = username
-        self.password = password
-        self.senderid = senderid
-        self.output = output
-        self.duplicatecheck = duplicatecheck
+    # ***********************QUICK SMS*******************************
 
     def send_quick_SMS(self, to: str, msg: str,):
         """Used to send a quick message to the provided mobile number."""
@@ -32,13 +27,36 @@ class Client:
         send_sms(self.userid, self.password, self.senderid,
                  self.output, self.duplicatecheck, msg, to=to, scheduleTime=scheduleTime)
 
+    # ***********************GROUP SMS*******************************
+
     def send_group_sms(self, group: str, msg: str):
         send_sms(self.userid, self.password, self.senderid,
                  self.output, self.duplicatecheck, msg, group=group)
 
+    def send_group_scheduled_sms(self, group: str, msg: str, scheduleTime):
+        send_sms(self.userid, self.password, self.senderid,
+                 self.output, self.duplicatecheck, msg, group=group, scheduleTime=scheduleTime)
+
+    def send_group_smartlink_sms(self, group: str, msg: str, smartLinkTitle):
+        send_sms(self.userid, self.password, self.senderid,
+                 self.output, self.duplicatecheck, msg, group=group, smartLinkTitle=smartLinkTitle)
+
+    # **********************SMS DELIVERY STATUS****************************
+
+
+class Client(message):
+
+    def __init__(self, username, password, senderid, output='json', duplicatecheck='true') -> None:
+        self.userid = username
+        self.password = password
+        self.senderid = senderid
+        self.output = output
+        self.duplicatecheck = duplicatecheck
+
 
 zettatel = Client("levin", 'n1xnS9qM', 'ZTSM')
 
-zettatel.send_quick_SMS('254768850685', "this is test from python package")
+zettatel.send_quick_SMS(
+    '254768850685', "this is test from python package")
 # zettatel.send_quick_smartlink_sms(
 #     '254768850685', 'Get compiled tender edition summary FREE . Register by visiting our website www.makkah.co.ke/tender.php or through our WhatsApp link below. To stop ,dial *456*9*5#', "Samrtlink Title test")
