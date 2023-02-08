8e32b524f1d4c15bf8a0d84df932181666718bf1
levin-mutai <levinmutai2@gmail.com>
2023-02-08 16:17:02 +0300

    finnished quick sms

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import requests
-from messages.resources import send_sms, send_smartlink_sms
+from messages.resources import send_sms
 import json
 
 
 class Client:
 
-    def __init__(self, username, password, mobile, senderid, output='json', duplicatecheck='true') -> None:
+    def __init__(self, username, password, senderid, output='json', duplicatecheck='true') -> None:
         self.userid = username
         self.password = password
         self.senderid = senderid
@@ -14,25 +14,31 @@ class Client:
 
     def send_quick_SMS(self, to: str, msg: str,):
         """Used to send a quick message to the provided mobile number."""
+
         send_sms(self.userid, self.password, self.senderid,
-                 self.output, self.duplicatecheck, to, msg)
+                 self.output, self.duplicatecheck, msg, to=to,)
 
     def send_quick_smartlink_sms(self, to: str, msg: str, smartLinkTitle: str):
         """Used to send a message with a link in it. The link will be shortened and assigned the provided smartlink title."""
 
         send_sms(self.userid, self.password, self.senderid,
-                 self.output, self.duplicatecheck, to, msg, smartLinkTitle=smartLinkTitle)
+                 self.output, self.duplicatecheck, msg, to=to, smartLinkTitle=smartLinkTitle)
         # send_smartlink_sms(self.userid, self.password, self.senderid,
         #                    self.output, self.duplicatecheck, to, msg, smartLinkTitle)
 
     def send_quick_sms_Schedule(self, to: str, msg: str, scheduleTime):
+        """Sends a scheduled SMS. 'scheduleTime' argument should be passed for it to work"""
+
         send_sms(self.userid, self.password, self.senderid,
-                 self.output, self.duplicatecheck, to, msg, scheduleTime=scheduleTime)
+                 self.output, self.duplicatecheck, msg, to=to, scheduleTime=scheduleTime)
+
+    def send_group_sms(self, group: str, msg: str):
+        send_sms(self.userid, self.password, self.senderid,
+                 self.output, self.duplicatecheck, msg, group=group)
 
 
-zettatel = Client("levin", 'n1xnS9qM',
-                  'ZTSM')
+zettatel = Client("levin", 'n1xnS9qM', 'ZTSM')
 
-# zettatel.send_quick_SMS('254768850685', "this is test from python package")
-zettatel.send_quick_smartlink_sms(
-    '254768850685', 'Get compiled tender edition summary FREE . Register by visiting our website www.makkah.co.ke/tender.php or through our WhatsApp link below. To stop ,dial *456*9*5#', "Samrtlink Title test")
+zettatel.send_quick_SMS('254768850685', "this is test from python package")
+# zettatel.send_quick_smartlink_sms(
+#     '254768850685', 'Get compiled tender edition summary FREE . Register by visiting our website www.makkah.co.ke/tender.php or through our WhatsApp link below. To stop ,dial *456*9*5#', "Samrtlink Title test")
--- messages/resources.py
+++ messages/resources.py
@@ -1,15 +1,14 @@
 import requests
 import re
-from utils import is_date
+from .utils import is_date
 
 
-def send_sms(userid, password,  senderid, output, duplicatecheck, to, msg: str, **kwargs):
+def send_sms(userid, password,  senderid, output, duplicatecheck, msg: str, **kwargs):
     extr = kwargs
     url = "https://portal.zettatel.com/SMSApi/send"
 
     payload = {'userid': userid,
                'password': password,
-               'mobile': to,
                'senderid': senderid,
                'msg': msg,
                'sendMethod': 'quick',
@@ -17,14 +16,19 @@ def send_sms(userid, password,  senderid, output, duplicatecheck, to, msg: str, 
                'output': output,
                'duplicatecheck': duplicatecheck}
 
-    if kwargs:
-        if extr['scheduleTime']:
-            if is_date(extr['scheduleTime']):
-                payload['scheduleTime'] = extr['scheduleTime']
-            else:
-                return ValueError("Invalid date and time")
-        elif extr['smartLinkTitle']:
-            payload['smartLinkTitle'] = extr['smartLinkTitle']
+    if kwargs.get('group'):
+        payload['group'] = extr['group']
+
+    elif kwargs.get('to'):
+        payload['mobile'] = extr['to']
+
+    if extr.get('scheduleTime'):
+        if is_date(extr['scheduleTime']):
+            payload['scheduleTime'] = extr['scheduleTime']
+        else:
+            return ValueError("Invalid date and time")
+    elif extr.get('smartLinkTitle'):
+        payload['smartLinkTitle'] = extr['smartLinkTitle']
 
     files = [
 
@@ -37,55 +41,3 @@ def send_sms(userid, password,  senderid, output, duplicatecheck, to, msg: str, 
     print(response.text)
 
     return response
-
-
-def send_smartlink_sms(userid, password, senderid, output, duplicatecheck, to, msg: str, smartLinkTitle):
-    url = "https://portal.zettatel.com/SMSApi/send"
-
-    payload = {'userid': userid,
-               'password': password,
-               'mobile': to,
-               'senderid': senderid,
-               'msg': msg,
-               'sendMethod': 'quick',
-               'msgType': 'text',
-               'output': output,
-               'duplicatecheck': duplicatecheck,
-               'trackLink': 'true',
-               'smartLinkTitle': smartLinkTitle
-               }
-    files = [
-
-    ]
-    headers = {}
-
-    response = requests.request(
-        "POST", url, headers=headers, data=payload, files=files)
-
-    print(response.text)
-
-    return response
-
-
-def send_scheduled_sms(userid, password,  senderid, output, duplicatecheck, to, msg: str):
-    url = "https://portal.zettatel.com/SMSApi/send"
-
-    payload = {'userid': 'levin',
-               'password': 'n1xnS9qM',
-               'mobile': '254768850685',
-               'senderid': 'ZTSMS',
-               'msg': 'This is testing schedule',
-               'sendMethod': 'quick',
-               'msgType': 'text',
-               'scheduleTime': '2019-09-16 13:24',
-               'output': 'json',
-               'duplicatecheck': 'true'}
-    files = [
-
-    ]
-    headers = {}
-
-    response = requests.request(
-        "POST", url, headers=headers, data=payload, files=files)
-
-    print(response.text)
